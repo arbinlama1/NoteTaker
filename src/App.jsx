60d6ceb61474a59d1c93a2c5177ed674055d8615
arbinlama1 <tamangarbin442@gmail.com>
2026-02-03 15:20:07 +0545

    installing the need dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
   <>
+  <h1 className='text-green-600 text-2xl font-bold tracking-[5px] capitalize p-6 border-2 w-fit rounded-full'>hello</h1>
   </>
   )
 }
